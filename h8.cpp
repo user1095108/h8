ea6e5e8ec59d088e1fd99055e763df5f1f77ea15
Janez Žemva <janezz55@gmail.com>
2022-04-27 12:04:07 +0200

    some fixes

---
+++ h8.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "h8.hpp"
+
+using namespace h8::literals;
+
+int main()
+{
+  enum
+  {
+    AAA = "AAA"_h8,
+    BB = "BB"_h8,
+  };
+
+  std::cout << h8::to_string(AAA) << std::endl;
+  std::cout << h8::to_string(BB) << std::endl;
+
+  //
+  std::cout << "AAA"_h8 << std::endl;
+  std::cout << "BB"_h8 << std::endl;
+
+  return 0;
+}
---
+++ h8.hpp
@@ -0,0 +1,65 @@
+#ifndef H8_HPP
+# define H8_HPP
+# pragma once
+
+#include <climits>
+#include <cstdint>
+
+#include <array>
+#include <string>
+#include <utility> // std::index_sequence
+
+namespace h8
+{
+
+using hash_t = std::uintmax_t;
+
+constexpr auto to_array(auto const h) noexcept
+{
+  return [&]<auto ...I>(std::index_sequence<I...>) noexcept
+    {
+      return std::array<char const, sizeof(h) + 1>{
+        char(h >> I * CHAR_BIT)...};
+    }(std::make_index_sequence<sizeof(h)>());
+}
+
+constexpr auto to_string(auto const h)
+{
+  return std::string(to_array(h).data());
+}
+
+template <typename T = hash_t>
+constexpr T hash(char const* const s, std::size_t const N) noexcept
+{
+  return [&]<auto ...I>(std::index_sequence<I...>) noexcept
+    {
+      return ((hash_t(I < N ? s[I] : 0) << I * CHAR_BIT) | ...);
+    }(std::make_index_sequence<sizeof(T)>());
+}
+
+template <typename T = hash_t, std::size_t N>
+constexpr auto hash(char const(&s)[N]) noexcept
+{
+  return hash<T>(s, N - 1);
+}
+
+template <typename T = hash_t>
+constexpr auto hash(std::string_view const& s) noexcept
+{
+  return hash<T>(s.data(), s.size());
+}
+
+namespace literals
+{
+
+constexpr auto operator"" _h8(char const* const s,
+  std::size_t const N) noexcept
+{
+  return hash(s, N);
+}
+
+}
+
+}
+
+#endif // H8_HPP
